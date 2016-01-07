254dfe056c1a7a80153b1f48c769d72df90565eb
annagaiel <annagaiel@gmail.com>
2016-01-07 07:19:41 -0800

    Add Game new,create,show on controller and setup its views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,25 @@
 class GamesController < ApplicationController
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(games_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  private
+
+  def games_params
+    params.require(:game).permit(:name)
+  end
 end
